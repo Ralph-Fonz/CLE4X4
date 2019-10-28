01f43a25ab69979f1ab1eac55a77bd68412426a9
Raphael Fonseca <raphael@fonseca.tech>
2019-10-28 16:10:10 -0400

    updated module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,3 +19,4 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
+export const routingComponents = [MainComponent, ForumComponent, StoreComponent, SideBarComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { StoreComponent } from './store/store.component';
-import { AboutInfoComponent } from './main/side-bar/about-info/about-info.component';
-import { PopularPostsComponent } from './main/side-bar/popular-posts/popular-posts.component';
-import { SubscribeComponent } from './main/side-bar/subscribe/subscribe.component';
-import { AdvertisingComponent } from './main/side-bar/advertising/advertising.component';
-import { TagsComponent } from './main/side-bar/tags/tags.component';
-import { InspirationsComponent } from './main/side-bar/inspirations/inspirations.component';
-import { ForumComponent } from './forum/forum.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    StoreComponent,
-    AboutInfoComponent,
-    PopularPostsComponent,
-    SubscribeComponent,
-    AdvertisingComponent,
-    TagsComponent,
-    InspirationsComponent,
-    ForumComponent
+    routingComponents,
+   
   ],
   imports: [
     BrowserModule,
